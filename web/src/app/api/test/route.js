810463931a9d57fcb8d83bcbf5b01220e1b7c30c
sugam-pokhrel <sugam.797402@memc.tu.edu.np>
2024-05-11 02:02:40 +0545

    server session added

--- web/src/app/api/test/route.js
+++ web/src/app/api/test/route.js
@@ -1,10 +1,16 @@
 import { NextResponse } from 'next/server'
- 
+import { getServerSession } from "next-auth/next"
+import { getSession } from 'next-auth/react'
 export async function GET(request) {
+  const session = await getServerSession(request)
+
+  console.log(session)
   return NextResponse.json({ msg: 'Hello from server' })
 }
 //above are for the post andget route hahahhah
 export async function POST(request) {
+  const session=await getSession({req:request})
+  console.log(session)
     let name=request.body.get('name')
 
     let url=request.body.url;
