4e72f9eb99c04f02b55e86050eb4890c4e5f7fcd
sugam-pokhrel <sugam.797402@memc.tu.edu.np>
2024-05-10 21:05:18 +0545

    made android login screen from chat gpt

--- android/trinetra/app/index.js
+++ android/trinetra/app/index.js
@@ -1,12 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
+import LoginForm from '../compos/LoginScreen'
 export default function index() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
     <Stack.Screen options={{ title: "Overview" }} />
 
-    <Text>Open up App.js to start I love yang sin on your app!</Text>
+    <LoginForm/>
   </View>
 
   )
