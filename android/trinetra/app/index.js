038db339b67cd248b15bd05419e1d1077d44a7c9
sugam-pokhrel <sugam.797402@memc.tu.edu.np>
2024-05-11 10:48:25 +0545

    adde camera to dashboard using iframe

--- android/trinetra/app/index.js
+++ android/trinetra/app/index.js
@@ -2,9 +2,11 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { useEffect } from 'react'
 import { Stack } from 'expo-router'
+
 import LoginForm from '../compos/LoginScreen'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 export default function index() {
+
 
   useEffect(()=>{
     AsyncStorage.getItem('user').then((user)=>{
@@ -12,7 +14,7 @@ export default function index() {
     }
     )
     
-  })
+  },[])
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
     <Stack.Screen options={{ title: "Login" }} />
--- web/src/app/dashboard/page.jsx
+++ web/src/app/dashboard/page.jsx
@@ -1,5 +1,16 @@
 const Dashboard = () => {
-  return <div>this is dashboard</div>;
+  return <div>this is dashboard
+
+
+
+<iframe
+      src=" http://127.0.0.1:5000/video_feed"
+      frameborder="0"
+      width="100%"
+      height="200px"
+     
+    />
+  </div>;
 };
 
 export default Dashboard;
