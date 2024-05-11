d34b49bc685e556bc782c572bd91f1cbc62b9fae
srijan <sapkotasrijan7@gmail.com>
2024-05-11 20:14:16 +0545

    fixed merge conflict

--- web/src/app/page.js
+++ web/src/app/page.js
@@ -1,17 +1,11 @@
 
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-import Landing from './landing/Landing';
-import Feature from './feature/Feature';
-import Footer from './footer/Footer';
 
 function page() {
   return (
-    <div className="pt-[10vh]">
+    <div className="">
       <Navbar />
-      <Landing />
-      <Feature />
-      <Footer />
       
     </div>
   )}
