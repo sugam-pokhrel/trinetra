f14f04cba40279ebcfca24b2908f9e80ab0b4079
nishant0603 <meprazhant@gmail.com>
2024-05-11 09:27:18 +0545

    added landing page

--- web/src/app/page.js
+++ web/src/app/page.js
@@ -1,11 +1,13 @@
 
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Landing from './landing/Landing';
 
 function page() {
   return (
     <div className="pt-[10vh]">
       <Navbar />
+      <Landing />
       
     </div>
   )}
