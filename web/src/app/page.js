1cb3948d6e2187b5b737ef37dd47db823e79d37f
nishant0603 <meprazhant@gmail.com>
2024-05-11 11:29:01 +0545

    added in page to load feature and landing

--- web/src/app/page.js
+++ web/src/app/page.js
@@ -2,12 +2,16 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Landing from './landing/Landing';
+import Feature from './feature/Feature';
+import Footer from './footer/Footer';
 
 function page() {
   return (
     <div className="pt-[10vh]">
       <Navbar />
       <Landing />
+      <Feature />
+      <Footer />
       
     </div>
   )}
