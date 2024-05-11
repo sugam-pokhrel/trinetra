f2d93478efe6382466292a17c126c27b1febccb6
sugam-pokhrel <sugam.797402@memc.tu.edu.np>
2024-05-11 22:31:26 +0545

    Added the file

--- web/src/app/dashboard/camera/page.jsx
+++ web/src/app/dashboard/camera/page.jsx
@@ -1,7 +1,21 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 
 const Camera = () => {
+  const [data, setData] = React.useState([]);
+  React.useEffect(() => {
+    fetch("/api/save", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.msg);
+      });
+  }, []);
   return (
     <div className="flex flex-col items-center justify-center h-screen w=full bg-gray-100 gap-10 pt-14">
       <h1 className="mb-4 text-2xl font-bold text-gray-700">Images Page</h1>
