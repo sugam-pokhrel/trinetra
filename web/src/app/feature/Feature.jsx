05d2234e0f6646d903e98012bc828cf9332a6b9a
nishant0603 <meprazhant@gmail.com>
2024-05-11 11:27:29 +0545

    added footer and features

---
+++ web/src/app/feature/Feature.jsx
@@ -0,0 +1,222 @@
+import React from "react";
+
+function Feature() {
+  return (
+    <div className="dark:bg-gray-100 dark:text-gray-800 " id="features">
+      <div className="px-4 py-16 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="max-w-3xl mx-auto text-center">
+          <h2 className="text-3xl font-extrabold sm:text-4xl">
+            All the features you want
+          </h2>
+        </div>
+        <dl className="mt-12 space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 lg:grid-cols-4 lg:gap-x-8">
+          <div className="flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+              className="flex-shrink-0 w-6 h-6 dark:text-violet-600"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 13l4 4L19 7"
+              ></path>
+            </svg>
+            <div className="ml-3">
+              <dt className="text-lg font-medium">
+                Facial Recognition Accuracy
+              </dt>
+              <dd className="mt-2 dark:text-gray-600">
+                Highlight the system's high accuracy in recognizing faces, even
+                in varying lighting conditions or from different angles.
+              </dd>
+            </div>
+          </div>
+          <div className="flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+              className="flex-shrink-0 w-6 h-6 dark:text-violet-600"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 13l4 4L19 7"
+              ></path>
+            </svg>
+            <div className="ml-3">
+              <dt className="text-lg font-medium">Real-Time Alerts</dt>
+              <dd className="mt-2 dark:text-gray-600">
+                Showcase the ability of the system to provide real-time alerts
+                when unauthorized individuals attempt to access restricted
+                areas.
+              </dd>
+            </div>
+          </div>
+          <div className="flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+              className="flex-shrink-0 w-6 h-6 dark:text-violet-600"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 13l4 4L19 7"
+              ></path>
+            </svg>
+            <div className="ml-3">
+              <dt className="text-lg font-medium">
+                Customizable Access Control
+              </dt>
+              <dd className="mt-2 dark:text-gray-600">
+                Emphasize the flexibility of the system in allowing
+                administrators to customize access permissions for different
+                individuals or groups.
+              </dd>
+            </div>
+          </div>
+          <div className="flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+              className="flex-shrink-0 w-6 h-6 dark:text-violet-600"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 13l4 4L19 7"
+              ></path>
+            </svg>
+            <div className="ml-3">
+              <dt className="text-lg font-medium">Integration Capabilities</dt>
+              <dd className="mt-2 dark:text-gray-600">
+                Highlight the system's compatibility with other security
+                systems, such as access control panels, alarm systems, or video
+                surveillance cameras.
+              </dd>
+            </div>
+          </div>
+          <div className="flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+              className="flex-shrink-0 w-6 h-6 dark:text-violet-600"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 13l4 4L19 7"
+              ></path>
+            </svg>
+            <div className="ml-3">
+              <dt className="text-lg font-medium">Scalability</dt>
+              <dd className="mt-2 dark:text-gray-600">
+                Showcase the system's scalability to accommodate different
+                numbers of users and locations, making it suitable for small
+                businesses to large enterprises.
+              </dd>
+            </div>
+          </div>
+          <div className="flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+              className="flex-shrink-0 w-6 h-6 dark:text-violet-600"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 13l4 4L19 7"
+              ></path>
+            </svg>
+            <div className="ml-3">
+              <dt className="text-lg font-medium">User-Friendly Interface</dt>
+              <dd className="mt-2 dark:text-gray-600">
+                Emphasize the intuitive and user-friendly interface of the
+                system, making it easy for administrators to set up and manage
+                access controls.
+              </dd>
+            </div>
+          </div>
+          <div className="flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+              className="flex-shrink-0 w-6 h-6 dark:text-violet-600"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 13l4 4L19 7"
+              ></path>
+            </svg>
+            <div className="ml-3">
+              <dt className="text-lg font-medium">Remote Access</dt>
+              <dd className="mt-2 dark:text-gray-600">
+                Showcase the ability for administrators to remotely monitor and
+                manage access controls, allowing for increased flexibility and
+                convenience.
+              </dd>
+            </div>
+          </div>
+          <div className="flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+              className="flex-shrink-0 w-6 h-6 dark:text-violet-600"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 13l4 4L19 7"
+              ></path>
+            </svg>
+            <div className="ml-3">
+              <dt className="text-lg font-medium">Customer Support</dt>
+              <dd className="mt-2 dark:text-gray-600">
+                Emphasize the availability of comprehensive customer support
+                services, including technical assistance, training resources,
+                and ongoing maintenance.
+              </dd>
+            </div>
+          </div>
+        </dl>
+      </div>
+    </div>
+  );
+}
+
+export default Feature;
